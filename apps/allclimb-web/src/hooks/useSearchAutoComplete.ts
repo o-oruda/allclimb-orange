00e844f93f5da18de12a13b4636b85a6616b2e17
yurim22 <dek1313@naver.com>
2024-10-27 19:28:06 +0900

    [feat] 클라이밍장 검색 기능 추가 이름, 지역 기준 검색 필터 필터 결과에 대한 하이라이트 기능 추가

---
+++ apps/allclimb-web/src/hooks/useSearchAutoComplete.ts
@@ -0,0 +1,43 @@
+import useSearchStore from '@/components/Search/features/useSearchStore';
+import { IGymInfo } from '@/models/common';
+import { useEffect, useState } from 'react';
+
+interface IAutoCompleteProps {
+	list: any[];
+	criteria: string[];
+}
+
+const useSearchAutoComplete = (props: IAutoCompleteProps) => {
+	const { list, criteria } = props;
+
+	const { searchValue } = useSearchStore();
+
+	// 자동완성 검색 결과
+	const [matchResult, setMatchResult] = useState<IGymInfo[]>([]);
+
+	useEffect(() => {
+		searchValue && fnMatchGround();
+	}, [searchValue]);
+
+	/**
+	 * 검색 결과 자동완성
+	 */
+	const fnMatchGround = () => {
+		if (!searchValue) setMatchResult([]);
+		else {
+			setMatchResult(
+				list.filter((gym) => {
+					for (const item of criteria) {
+						if (gym[item].includes(searchValue)) {
+							return true;
+						}
+					}
+				}),
+			);
+		}
+	};
+
+	return { fnMatchGround, matchResult };
+};
+
+export default useSearchAutoComplete;
--- apps/allclimb-web/src/models/const.ts
+++ apps/allclimb-web/src/models/const.ts
@@ -1,3 +1,12 @@
+// 전체 클라이밍장 임시
+export const climbingGround: { name: string; location: string }[] = [
+	{ name: '어웨이크클라이밍 타임월드점', location: '대전시 서구 둔산동' },
+	{ name: '웨이브락 서면점', location: '부산시 부산진구 부전동' },
+	{ name: '스톤즈클라이밍', location: '서울시 관악구 봉천동' },
+	{ name: '오프더월클라이밍', location: '서울시 용산구 이태원동' },
+];
+
+// 지역 이름
 export const REGIONS = [
 	{ name: '서울', id: 'seoul' },
 	{ name: '경기도', id: 'gyeonggi' },
@@ -8,6 +17,7 @@ export const REGIONS = [
 	{ name: '제주도', id: 'jeju' },
 ];
 
+// 네비게이션 아이템
 export const NAV_ITEMS: {
 	path: string;
 	label: string;
