d4fbee654b34bc8420d4862799b84ab12d2edf19
OHKuenYoung <rppjpp@naver.com>
2024-11-10 22:59:22 +0900

    [OORUDA-37] 기존 작업물 이동

--- apps/allclimb-web/src/pages/Tickets/TicketPage.tsx
+++ apps/allclimb-web/src/pages/Tickets/TicketPage.tsx
@@ -1,12 +1,44 @@
-import BottomNavi from '@/components/BottomNavi/BottomNavi';
+import React from "react";
 
-const TicketPage = () => {
-	return (
-		<div>
-			<div>ticket</div>
-			<BottomNavi />
-		</div>
-	);
-};
+import styles from './Ticket.module.scss';
+import classNames from 'classnames/bind';
+import BottomNavi from "components/BottomNavi/BottomNavi";
 
-export default TicketPage;
+const cx = classNames.bind(styles);
+
+const Ticket = () => {
+    return (
+        <>
+            <div className={cx('ticket')}>
+                <h2 className={cx('ticket__title')}>
+                    내 이용권 <em>1</em>
+                </h2>
+
+                <ul className={cx('ticket-tab')} role="tablist">
+                    <li className={cx('ticket-tab__item')} role="tab" aria-selected="true">
+                        <button
+                            className={cx('ticket-tab__button')}
+                            type="button"
+                        >
+                            이용내역
+                        </button>
+                    </li>
+
+                    <li className={cx('ticket-tab__item')} role="tab" aria-selected="true">
+                        <button
+                            className={cx('ticket-tab__button')}
+                            type="button"
+                        >
+                            이용내역
+                        </button>
+                    </li>
+                </ul>
+
+                
+                <BottomNavi/>
+            </div>
+        </>
+    )
+}
+
+export default Ticket;
