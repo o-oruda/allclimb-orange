3c4280fbd95333310904710c6a34086f08d77c66
yurim <yrseo1216@gmail.com>
2024-12-10 23:57:01 +0900

    [fix] eslint 설정 수정

--- apps/allclimb-web/eslint.config.js
+++ apps/allclimb-web/eslint.config.js
@@ -1,13 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{ languageOptions: { globals: globals.browser } },
+	{
+		rules: {
+			quotes: ['error', 'single'],
+			'no-duplicate-imports': 'error',
+			'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+			'no-unused-vars': 'error',
+			'no-multiple-empty-lines': 'error',
+			'react/react-in-jsx-scope': 'off',
+		},
+	},
+	{
+		plugins: {
+			react: pluginReact,
+			'@typescript-eslint': tseslint,
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
 ];
